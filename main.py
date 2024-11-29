8210bb503618db255da3df63800078bf917fbd38
Italo Sanfer <italo2sanfer@gmail.com>
2024-11-28 21:46:01 -0300

    FastAPI getting up.

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 from file_handler import ProductFH
 from models import Product
+from enum import Enum
+from fastapi import FastAPI
 
-pfh1 = ProductFH(Product(1, "notebook"))
-pfh1.add_in_file()
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
-pfh2 = ProductFH(Product(2, "mouse"))
-pfh2.add_in_file()
+app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/file_handler/go")
+async def file_handler_go():
+    pfh1 = ProductFH(Product(1, "notebook"))
+    pfh1.add_in_file()
+    pfh2 = ProductFH(Product(2, "mouse"))
+    pfh2.add_in_file()
+    return {"user_id": "The FileHandler went."}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+# Command for perform
+# uvicorn main:app --reload
