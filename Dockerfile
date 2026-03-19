733b85b57cd8b6f6481d1fc1359ab252215c59a7
Italo dos Santos Ferreira <italo2sanfer@gmail.com>
2026-03-19 17:11:31 +0000

    Configuring a specific connection for Cloud Run.

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,5 @@ COPY entrypoint.sh ./entrypoint.sh
 RUN chmod +x ./entrypoint.sh
 
 EXPOSE 8080
-ENTRYPOINT ["./entrypoint.sh"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
+#ENTRYPOINT ["./entrypoint.sh"]
--- database.py
+++ database.py
@@ -1,18 +1,34 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from google.cloud.sql.connector import Connector
 
+INSTANCE_CONNECTION_NAME = "projeto:regiao:instancia"
 DB_USER = os.getenv("DB_USER", "usuario")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "senha123")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "meubanco")
 
-DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# Inicializa o Conector
+connector = Connector()
+
+def getconn():
+    conn = connector.connect(
+        INSTANCE_CONNECTION_NAME,
+        "pg8000", # ou "pymysql" para MySQL
+        user=DB_USER,
+        password=DB_PASSWORD,
+        db=DB_NAME
+    )
+    return conn
+
+# Cria o pool de conexão com SQLAlchemy
+engine = sqlalchemy.create_engine(
+    "postgresql+pg8000://",
+    creator=getconn,
 )
 
-engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 Base = declarative_base()
 
